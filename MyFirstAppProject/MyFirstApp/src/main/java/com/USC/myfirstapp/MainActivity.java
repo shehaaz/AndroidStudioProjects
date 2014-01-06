84db4fd6e5b6e97bb24f578b2b605a0dc2e38070
shehaaz <shehaaz@gmail.com>
2014-01-06 18:50:02 -0500

    Initialized app with look and feel

--- MyFirstAppProject/MyFirstApp/src/main/java/com/USC/myfirstapp/MainActivity.java
+++ MyFirstAppProject/MyFirstApp/src/main/java/com/USC/myfirstapp/MainActivity.java
@@ -1,17 +1,22 @@
 package com.USC.myfirstapp;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+
+    private Time time;
+    private static Button bluetooth_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +28,25 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        time = new Time();
+        time.setToNow();
+
+        bluetooth_button = (Button) findViewById(R.id.button_bluetooth);
+
+        bluetooth_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(), "Bluetooth Button clicked at"+ " "+ getTime(),
+                            Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
+    public String getTime () {
+
+        return time.hour + ":" + time.minute + ":" + time.second;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,7 +62,29 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
+
             case R.id.action_settings:
+                return true;
+
+            case R.id.patient_profile:
+                Toast.makeText(getApplicationContext(), "Open Patient Profile",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.ask_patient:
+                Toast.makeText(getApplicationContext(), "Open Ask Patient",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.medical_recs:
+                Toast.makeText(getApplicationContext(), "Open Medical Records",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.curr_patient:
+                Toast.makeText(getApplicationContext(), "Open Current Patient",
+                        Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.vital_signs:
+                Toast.makeText(getApplicationContext(), "Open Vital Signs",
+                        Toast.LENGTH_LONG).show();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -58,8 +102,10 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
             return rootView;
         }
+
     }
 
 }
